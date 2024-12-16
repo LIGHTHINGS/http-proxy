6708bcf89d97f1451f33aa85b45926e19fb7a2cd
Dare Omotayo <dareomotayo32@gmail.com>
2024-12-16 01:20:29 +0100

    modified the  bearer for the authorization

--- app.js
+++ app.js
@@ -13,10 +13,15 @@ app.use(cors({
 // Proxy route
 app.all('/api*', async (req, res) => {
     const proxyOrigin = process.env.Origin;
+    const token  = req.headers['Authorization'].split('')[1]
     try {
         const subRoute = req.path.split('api')[1];
         const backendResponse =  await axios.post(`http://stagingapi.vampfi.com/api${subRoute}`, req.body, {
-            headers: { 'Content-Type': 'application/json', 'Origin': proxyOrigin },
+            headers: { 
+                'Content-Type': 'application/json',
+                'Origin': proxyOrigin,
+                Authorization: `Bearer ${token}`
+            },
             timeout: 10000
         });
         // Send the backend's response to the client
